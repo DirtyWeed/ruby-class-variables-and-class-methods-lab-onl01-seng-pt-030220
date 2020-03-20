e590e701e84b00902000ec99746ae0d03ceb96f9
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-20 14:16:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 class Song
   @@count = 0
   @@genres = []
+  @@artists = []
   attr_accessor :name :artist :genre
   
   def initialize
     @@count += 1
     @@genres << :genre
+    @@artists << :artist
+    @@genre_count = {}
   end
   
   def self.count 
@@ -15,5 +18,16 @@ class Song
   def self.genres
     @@genres.uniq!
   end
+  
+  def self.artists
+    @@artists.uniq!
+  end
+  
+  def self.genre_count
+  end
+
+  def self.artist_count
+  end
+
 
 end
